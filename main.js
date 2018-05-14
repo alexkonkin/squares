a860ae5229e8aa6fd98f2f6aaa2d72827d514a79
alexkonkin <alexkonkin@gmail.com>
2018-05-14 22:27:05 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+//http://jsfiddle.net/m1erickson/myHDW/
+
+var currentPos = 0;
+
+function animate() {  
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');  
+  ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientWidth);  
+  ctx.fillRect(320, currentPos, 20, 20);
+  currentPos += 1;
+  if(currentPos >= canvas.clientHeight) {
+    currentPos = 0;
+  }
+  requestAnimationFrame(animate);
+}
+
+document.body.onload = animate;
+
+
